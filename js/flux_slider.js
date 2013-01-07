1796073abc6a4e5533359a7390c1a2c06f1df8fc
Ray Nimmo <ray@junglecreative.com>
2013-01-07 15:01:27 +0700

    Issue #1881400 : Fix for translation errors with multiple code style revisions.

--- js/flux_slider.js
+++ js/flux_slider.js
@@ -14,22 +14,21 @@
    Drupal.behaviors.fluxSlider = {
      attach: function (context, settings) {
        // Get the settings.
-
        var flux_autoplay = Drupal.settings.flux_slider.flux_autoplay;
-       var flux_autoplay_bool = (flux_autoplay == 'on' ? false : true);
+       var flux_autoplay_bool = (flux_autoplay == 1 ? true : false);
 
        var flux_transition = Drupal.settings.flux_slider.flux_transition;
 
        var flux_delay = Drupal.settings.flux_slider.flux_delay;
 
        var flux_pagination = Drupal.settings.flux_slider.flux_pagination;
-       var flux_pagination_bool = (flux_pagination == 'show' ? true : false);
+       var flux_pagination_bool = (flux_pagination == 1 ? true : false);
 
        var flux_controls = Drupal.settings.flux_slider.flux_controls;
-       var flux_controls_bool = (flux_controls == 'show' ? true : false);
+       var flux_controls_bool = (flux_controls == 1 ? true : false);
 
        var flux_captions = Drupal.settings.flux_slider.flux_captions;
-       var flux_captions_bool = (flux_captions == 'show' ? true : false);
+       var flux_captions_bool = (flux_captions == 1 ? true : false);
 
        var flux_width = Drupal.settings.flux_slider.flux_width;
        var flux_height = Drupal.settings.flux_slider.flux_height;
@@ -46,8 +45,10 @@
           height: flux_height,
         });
         // Adjust container height to cope with bug http://drupal.org/node/1880556.
+        // Additional height required if pagination controls are shown.
+        flux_container_height = (flux_pagination_bool ? (parseInt(flux_height, 10))+40 : flux_height);
         jQuery("div#slider").css({
-          "height":flux_height,
+          "height":flux_container_height,
           "overflow":"hidden"
         });
        }
